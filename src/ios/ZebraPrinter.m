c5044325e44cd26c1aa452a78625e365a9b5400d
am934743 <maxorpharmacy@mac-mini.maxor.com>
2016-12-07 14:46:16 -0600

    update array for print Object

--- src/ios/ZebraPrinter.m
+++ src/ios/ZebraPrinter.m
@@ -9,6 +9,7 @@
 
 - (void) sendZplOverBluetooth:(CDVInvokedUrlCommand*)command {
     
+    NSArray* printItems = command.arguments;
         
         //Dispatch this task to the default queue
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^ {
