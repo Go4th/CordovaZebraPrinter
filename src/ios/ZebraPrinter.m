18f119edfe5c1490a28ee6bbcef36ea8ba9e37b4
am934743 <maxorpharmacy@mac-mini.maxor.com>
2016-12-09 14:59:54 -0600

    Consolidating fields into one string

--- src/ios/ZebraPrinter.m
+++ src/ios/ZebraPrinter.m
@@ -10,13 +10,16 @@
 - (void) sendZplOverBluetooth:(CDVInvokedUrlCommand*)command {
     
     NSDictionary* printItems = command.arguments[0];
-    NSNumber* printCount = printItems [@"scriptCount"];
-    NSString* printTitle = printItems [@"title"];
-    NSString* printPayMethod = printItems [@"ticketPaymentMethod"];
+    // NSNumber* printCount = printItems [@"scriptCount"];
+    // NSString* printTitle = printItems [@"title"];
+    // NSString* printPayMethod = printItems [@"ticketPaymentMethod"];
     NSNumber* printTransId = printItems [@"transactionId"];
+    NSString* printBody = printItems [@"printBody"]
     
     // NSString* finalPrint = [NSString stringWithFormat:@"^XA\r\n^FO50,50\r\n^FD%@^FS\r\n^FO50,150\r\n^FDPaid For: %@\r\n^FO50,250\r\n^FDItems On This Ticket: %@\r\n^FO50,350\r\n^FO100,100^BY3^B1N,N,150,Y,N^FD%@^FS^XZ", printTitle, printPayMethod, printCount, printTransId];
     NSString* finalPrint = [NSString stringWithFormat:@"^XA^CF0,75^A2N,50,50^FO200,100^FD%@^FS^A2N,40,40^FO250,175^FDPaid For: %@^FS^A2N,40,40^FO225,225^FDItems On This Ticket: %@^FS^FO275,300^BY3^B1N,N,150,Y,N^FD%@^FS^XZ", printTitle, printPayMethod, printCount, printTransId];
+    // Print Body String
+    NSString* finalPrintBody = [NSString stringWithFormat:@"^XA^CF0,75^A2N,50,50^FO200,100^FD%@^FS^FO275,300^BY3^B1N,N,150,Y,N^FD%@^FS^XZ", printBody, printTransId];
     NSLog(@"this is the variable value: %@",printItems[@"scriptCount"]);
 
         
